568de876e23c9e5ba4821f0b9ddf7e05217e5e2a
lokeshllkumar <lokeshkumar2015.2004@gmail.com>
2024-10-21 11:50:40 +0530

    created server handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"net"
 	"net/http"
 	"time"
 
@@ -9,15 +10,26 @@ import (
 	"github.com/lokeshllkumar/load-balancer/internal/balancer"
 )
 
-func RegisterRoutes(router *gin.Engine, pool *backend.BackendPool) {
+func RegisterRoutes(router *gin.Engine, lb *balancer.LoadBalancer, strategy string) {
 	router.GET("/load-balancer", func(c *gin.Context) {
-		backends := pool.GetAliveBackends()
+		backends := lb.Pool.GetAliveBackends()
 		if len(backends) == 0 {
 			c.JSON(http.StatusServiceUnavailable, gin.H{"error": "No backends available"})
 			return
 		}
-		backend := balancer.NewRoundRobin().GetNextBackendRR(backends)
-		c.Redirect(http.StatusTemporaryRedirect, backend.URL)
+		
+		var selectedBackend *backend.Backend
+		clientIP := getClientIP(c.Request)
+
+		switch strategy {
+		case "round-robin":
+			selectedBackend = balancer.GetNextBackendRR(backends)
+		case "least-connections":
+			selectedBackend = balancer.GetNextBackendLC(backends)
+		case "sticky":
+			selectedBackend = lb.GetBackendSS(clientIP)
+		}
+		c.Redirect(http.StatusTemporaryRedirect, selectedBackend.URL)
 	})
 }
 
@@ -29,4 +41,12 @@ func CreateServer(router *gin.Engine) *http.Server {
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
+}
+
+func getClientIP(req *http.Request) string {
+	ip, _, err := net.SplitHostPort(req.RemoteAddr)
+	if err != nil {
+		return req.RemoteAddr
+	}
+	return ip
 }
